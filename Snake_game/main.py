0984daf42ad3227216829ebe761ade3a1b20bb20
Diego <diegotorres2.dd@gmail.com>
2024-02-20 02:59:50 +0100

    Added changes

---
+++ Snake_game/main.py
@@ -0,0 +1,24 @@
+from turtle import Screen, Turtle
+from snake import Snake
+import time
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    screen.listen()
+    screen.onkey(snake.up, "w")
+    screen.onkey(snake.down, "s")
+    screen.onkey(snake.left, "a")
+    screen.onkey(snake.right, "d")
+
+screen.exitonclick()
